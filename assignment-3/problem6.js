256aaf5b835061733332fce263c23fcc0062ae61
bhabesh2 <jsarma058@gmail.com>
2023-06-22 14:00:43 +0530

    solution added

---
+++ assignment-3/problem6.js
@@ -0,0 +1,23 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except
+for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only
+constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+*/
+
+var singleNumber = function (nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+  return result;
+};
+
+const nums = [2, 2, 1];
+
+console.log(singleNumber(nums));
